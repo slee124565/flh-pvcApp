61ba4732281f7e693674b5ba50873dfea4038bf6
Lee Shiueh <lee.shiueh@gmail.com>
2016-11-17 16:32:51 +0800

    yo angular pvc initial project

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pvcApp
+ * @description
+ * # pvcApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pvcApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
