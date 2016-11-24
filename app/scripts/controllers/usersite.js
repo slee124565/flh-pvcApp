e153ad99dcefc0d902d0bbce768740f4e54a6c9b
Lee Shiueh <lee.shiueh@gmail.com>
2016-11-24 17:32:57 +0800

    add amCharts initial action

--- app/scripts/controllers/usersite.js
+++ app/scripts/controllers/usersite.js
@@ -51,4 +51,104 @@ angular.module('pvcApp')
             }
         }
     };
+    
+    // amCharts 
+AmCharts.makeChart("amchart1", {
+  "type": "serial",
+  "addClassNames": true,
+  "theme": "light",
+    "legend": {
+        "equalWidths": false,
+        "useGraphSettings": true,
+        "valueAlign": "left",
+        "valueWidth": 120
+    },
+  "balloon": {
+    "adjustBorderColor": false,
+    "horizontalPadding": 10,
+    "verticalPadding": 8,
+    "color": "#ffffff"
+  },
+  "balloonDateFormat" : "JJ:NN",
+
+  "dataProvider": JSON.parse($scope.site.content.amchart.data) ,
+    /*
+  "dataLoader": {
+    "url": 'http://211.75.6.108:8080/appeng/amchart/3/',
+  },*/
+  "valueAxes": [{
+    "id": "energyAxis",           
+    "axisAlpha": 0,
+     "gridAlpha":0,
+    "position": "left",
+     "title": "Energy(Wh)",},
+    {
+     "id": "uvAxis",           
+     "axisAlpha": 0,
+     "gridAlpha":0,
+     "position": "right",
+     "title": "UV",
+     
+    },
+    ],
+  "startDuration": 1,
+  "graphs": [{
+    "alphaField": "alpha",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
+    "fillAlphas": 1,
+    "type": "column",
+     "title": "Energy",
+    "valueField": "energy",
+     "valueAxis": "energyAxis",
+    "dashLengthField": "dashLengthColumn"
+  }, {
+    "id": "graph3",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]<br><span style='font-size:20px;'>[[value]](max)</span> [[additional]]</span>",
+    "bullet": "triangleUp",
+    "lineThickness": 2,
+    "bulletSize": 7,
+    "bulletBorderAlpha": 1,
+    "bulletColor": "#A9F5A9",
+                //"fillColors" : "#A9F5A9",
+    "useLineColorForBulletBorder": true,
+    "bulletBorderThickness": 3,
+    "fillAlphas": 0,
+    "lineAlpha": 1,
+    "title": "UV",
+    "valueField": "uv",
+    "valueAxis": "uvAxis",
+  }],
+  "dataDateFormat": "YYYY-MM-DD JJ:NN:SS",
+  "categoryField": "date",
+  "categoryAxis": {
+    "dateFormats": [{
+      		"period":'hh', 
+      		"format":'JJ:NN'
+    	}, {
+          	"period": 'JJ', 
+          	"format": 'JJ:NN'
+    	}, {
+            "period": "DD",
+            "format": "MMM DD"
+        }, {
+            "period": "WW",
+            "format": "MMM DD"
+        }, {
+            "period": "MM",
+            "format": "MMM"
+        }, {
+            "period": "YYYY",
+            "format": "YYYY"
+        }],
+    "parseDates": true,
+    "minPeriod" : "hh",
+    "gridPosition": "start",
+    "axisAlpha": 0,
+    "tickLength": 0
+  },
+  "export": {
+    "enabled": true
+  }
+});    
+    // .amCharts
   }]);
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/amcharts3/amcharts/amcharts.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
